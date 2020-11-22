8188db0227f811f9f33ef6c0fcb6cb99d51921e7
Bruno Rilla <brilla@Lenovo.com>
2020-11-22 01:15:22 -0300

    Adding session helper method

---
+++ Helpers/SessionHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Satish.Helpers
+{
+    public static class SessionHelper
+    {
+        public static void SetObjectAsJson(this ISession session, string key, object value)
+        {
+
+        }
+    }
+}
