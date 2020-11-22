49be09809e8da8acea4c9bba3c32ce98f975bf00
Bruno Rilla <brilla@Lenovo.com>
2020-11-22 17:00:58 -0300

    adding further styles

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -6,16 +6,9 @@
 var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
     slidesPerView : 1,
 
     // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
 
     // And if we need scrollbar
     scrollbar: {
